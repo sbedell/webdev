328832f55d9c47b5bafc708833d55b795466b673
Steve Bedell <bedell.369@osu.edu>
2016-10-18 13:37:17 -0400

    Finished Angular2 tutorial thru Mutliple Components section

--- angular2-tutorial/app/app.component.ts
+++ angular2-tutorial/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-
-export class Hero {
-  id: number;
-  name: string;
-}
+import { Hero } from './hero';
 
 const HEROES: Hero[] = [
   { id: 1, name: 'Windstorm' },
@@ -25,20 +21,14 @@ const HEROES: Hero[] = [
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes" [class.selected]="hero === selectedHero" 
-       (click)="onSelect(hero)">
+      <li *ngFor="let hero of heroes"
+        [class.selected]="hero === selectedHero" 
+        (click)="onSelect(hero)" >
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-    </div>
-  `,
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+    `,
   // styleUrls: ['heroes.component.css'],
   styles: [`
   .selected {
