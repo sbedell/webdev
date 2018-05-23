27241201ccef3d2b7ab5c89f229b8e9d23a98326
Steve B <bedell.369@osu.edu>
2018-05-23 00:11:48 -0400

    Making a webserver directory to house some serviceWorker demos, also moving old SW folders over too

---
+++ webserver/server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+const helmet = require('helmet');
+
+var app = express();
+app.use(helmet());
+
+// set the static files location; /public/img will be /img for users
+// NOTE - Not using right now
+// app.use(express.static(__dirname + '/public'));
+
+// Error handling:
+app.use(function(err, req, res, next) {
+	// prints stack trace to the console
+	console.error(err.stack);
+
+	// Sends generic error page to the user
+    res.status(500).send('Error!');
+});
+
+app.get('/', function(request, response) {
+	response.send("Hello World");
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log('Node app is running on port', port);
+});
