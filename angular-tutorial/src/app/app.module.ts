9d2b22cbd890b632217944b26f9e3aa448382cb6
Steve B <bedell.369@osu.edu>
2018-11-23 20:28:40 -0500

    Updating Angular tutorial with Hero Search component. Finished the Angular tutorial! (HTTP section was the last section).

--- angular-tutorial/src/app/app.module.ts
+++ angular-tutorial/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { InMemoryDataService }  from './in-memory-data.service';
@@ -19,7 +20,8 @@ import { InMemoryDataService }  from './in-memory-data.service';
     HeroesComponent,
     MessagesComponent,
     HeroDetailComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
--- angular-tutorial/src/app/hero.service.ts
+++ angular-tutorial/src/app/hero.service.ts
@@ -4,22 +4,19 @@ import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from './hero';
-import { heroList } from './mock-heroes';
 import { MessageService } from './message.service';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class HeroService {
+  private heroesUrl = 'api/heroes';  // URL to web api  
+
   constructor(private http: HttpClient, 
     private messageService: MessageService) { }
-
-  private heroesUrl = 'api/heroes';  // URL to web api  
-  private httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-  };
-
+    
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
       .pipe(tap(_ => this.log('fetched heroes')),
@@ -39,7 +36,7 @@ export class HeroService {
 
   /** PUT: update the hero on the server */
   updateHero (hero: Hero): Observable<any> {
-    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+    return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
       tap(_ => this.log(`Updated hero id=${hero.id}, name=${hero.name}`)),
       catchError(this.handleError<any>('updateHero'))
     );
@@ -47,7 +44,7 @@ export class HeroService {
 
   /** POST: add a new hero to the server */
   addHero (hero: Hero): Observable<Hero> {
-    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
       tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
       catchError(this.handleError<Hero>('addHero'))
     );
@@ -58,7 +55,7 @@ export class HeroService {
     const id = typeof hero === 'number' ? hero : hero.id;
     const url = `${this.heroesUrl}/${id}`;
 
-    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+    return this.http.delete<Hero>(url, httpOptions).pipe(
       tap(_ => this.log(`deleted hero id=${id}`)),
       catchError(this.handleError<Hero>('deleteHero'))
     );
