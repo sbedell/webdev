677966ad0b2de50ba98ca8f211e995d0f2c0f96c
Steve B <bedell.369@osu.edu>
2019-01-31 12:58:07 -0500

    Adding Kraken Websocket API demo, CSS Animation demo, and a palidrome.js demo

---
+++ JavaScript/kraken-websocket-demo.js
@@ -0,0 +1,41 @@
+/**
+ * Kraken Websockets Demo
+ * https://www.kraken.com/features/websocket-api
+ * 
+ * Resources:
+ * https://developer.mozilla.org/en-US/docs/Web/API/WebSocket
+ * https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_client_applications
+ */
+
+// Create WebSocket connection.
+// Sandbox URL: ws-sandbox.kraken.com
+// Production URL: ws.kraken.com
+var socket = new WebSocket("wss://ws.kraken.com");
+
+// From Kraken: "Once the socket is open you can subscribe to a channel by sending a subscribe request message."
+socket.addEventListener('open', function(event) {
+  let subData = {
+    "event": "subscribe",
+    "pair": [
+      "XBT/USD",
+      "ETH/USD",
+      "LTC/USD" 
+    ],
+    "subscription": {
+      "name": "ticker"
+    }
+  };
+
+  socket.send(JSON.stringify(subData));
+});
+
+// Listen for messages
+// socket.addEventListener('message', function(event) {
+//   console.log('Message from server ', event.data);
+// });
+socket.onmessage = function(event) {
+  console.log('Message from server ', event.data);
+};
+
+// Call this to close the connection...but when?
+// socket.close();
