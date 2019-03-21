1a23ab275689339ca6b02f0ab892724f6bd79983
Steve B <bedell.369@osu.edu>
2019-03-21 01:33:47 -0400

    Deleting Lightning Components tutorial, Apex & Lightning are trash

--- LightningComponentsTrailhead/CampingList/campingApp.app
+++
@@ -1,9 +0,0 @@
-<aura:application >
-    <ltng:require styles="{!$Resource.SLDS212 +
-         '/assets/styles/salesforce-lightning-design-system.css'}"/>
- 
-    <div class="slds">
-        <!-- This component is the real "app" -->
-        <c:campingList />
-    </div>
-</aura:application>
--- LightningComponentsTrailhead/CampingList/campingListFormController.js
+++
@@ -1,9 +0,0 @@
-({
-    // Previously Called createCampingItem
-	submitForm: function(component, event, helper) {        
-        if (helper.validateItem(component)) {
-            var item = component.get("v.newItem");
-            helper.createItem(component, item);
-        }
-    },
-})
