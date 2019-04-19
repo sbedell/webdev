8d9fa5d35330fae5ed66fefbadec2fca02ea0321
Steve B <bedell.369@osu.edu>
2019-04-19 19:25:52 -0400

    Updating sha1hashgen and filereader

--- node.js/filereader.js
+++ node.js/filereader.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 if (!process.argv[2]) {
   console.error("\nPlease input a filename");
   // console.error("Expected usage:");
-  // console.error("`node sha1hashgen.js somefilename`");
   return;
 } else {
     const filename = process.argv[2];
@@ -13,7 +12,7 @@ if (!process.argv[2]) {
         if (!err) {
             console.log('received data: \n' + data);
 
-            console.log(data.split("\n"));
+            // console.log(data.split("\n"));
         } else {
             console.error(err);
         }
--- node.js/sha1hashgen.js
+++ node.js/sha1hashgen.js
@@ -1,24 +1,29 @@
 const crypto = require('crypto');
 const fs = require('fs');
 
-if (process.argv[2] == null) {
+if (process.argv[2]) {
+    const filename = process.argv[2];
+    const input = fs.createReadStream(filename);
+    let hash = crypto.createHash('sha1');
+
+    input.on('readable', function() {
+      let data = input.read();
+
+      if (data) {
+        hash.update(data);
+      } else {
+        let fileSHA1 = hash.digest('hex').toUpperCase();
+        console.log(`\nThe SHA1 hash of ${filename} is \n\n${fileSHA1}`);
+        
+        if (process.argv[3]) {
+            let hashToCheck = String(process.argv[3]).trim().toUpperCase();
+            console.log("Hash match? ", fileSHA1 === hashToCheck);
+        }
+      }
+    });
+} else {
   console.error("\nPlease input a filename");
   console.error("Expected usage:");
-  console.error("`node sha1hashgen.js somefilename`");
+  console.error("node sha1hashgen.js somefilename.filepath");
   return;
 }
-
-const filename = process.argv[2];
-const input = fs.createReadStream(filename);
-let hash = crypto.createHash('sha1');
-
-input.on('readable', function() {
-  let data = input.read();
-
-  if (data) {
-    hash.update(data);
-  } else {
-    console.log(`\nThe SHA1 hash of ${filename} is`);
-    console.log(`${hash.digest('hex')}`);
-  }
-});
