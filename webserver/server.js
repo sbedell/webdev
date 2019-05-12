264ee5b808950f033a6ed57372da48aa86f2a347
Steve B <bedell.369@osu.edu>
2019-05-12 16:22:36 -0400

    Minor updates to web server

--- webserver/server.js
+++ webserver/server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const helmet = require('helmet');
 
-var app = express();
+let app = express();
 app.use(helmet());
 
 // set the static files location; /public/img will be /img for users
@@ -11,18 +11,16 @@ app.use(express.static(__dirname + '/public'));
 
 // Error handling:
 app.use(function(err, req, res, next) {
-	// prints stack trace to the console
 	console.error(err.stack);
 
 	// Sends generic error page to the user
-    res.status(500).send('Error!');
+  res.status(500).send('Error!');
 });
 
 app.get('/', function(request, response) {
-	//response.send("Hello World");
 	response.send(`Hello World! We are using Node.js ${process.versions.node}
 		and OpenSSL ${process.versions.openssl} running on ${process.platform} ${process.arch}
-		for ${process.uptime()} seconds`);
+		for ${process.uptime()} seconds.`);
 });
 
 const port = process.env.PORT || 8080;
