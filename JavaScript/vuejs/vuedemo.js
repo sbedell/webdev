085d1603aac1e43267699d0c7d6ccfcfda46fabb
Steve B <bedell.369@osu.edu>
2019-09-04 17:46:51 -0400

    Updating Vue demos - adding some new stuff and renaming a few files

--- JavaScript/vuejs/vuedemo.js
+++ JavaScript/vuejs/vuedemo.js
@@ -5,21 +5,21 @@ var app = new Vue({
     }
 });
 
-var app2 = new Vue({
+let app2 = new Vue({
   el: '#app-2',
   data: {
     message: 'You loaded this page on ' + new Date().toLocaleString()
   }
 });
 
-var app3 = new Vue({
+let app3 = new Vue({
   el: '#app-3',
   data: {
     seen: true
   }
 });
 
-var app4 = new Vue({
+let app4 = new Vue({
   el: '#app-4',
   data: {
     todos: [
@@ -30,19 +30,19 @@ var app4 = new Vue({
   }
 });
 
-var app5 = new Vue({
+let app5 = new Vue({
   el: '#app-5',
   data: {
     message: 'Hello Vue.js!'
   },
   methods: {
-    reverseMessage: function () {
+    reverseMessage: function() {
       this.message = this.message.split('').reverse().join('');
     }
   }
 });
 
-var app6 = new Vue({
+let app6 = new Vue({
   el: '#app-6',
   data: {
     message: 'Type in here'
@@ -66,4 +66,11 @@ let app7 = new Vue({
       { id: 2, text: 'Whatever else humans are supposed to eat' }
     ]
   }
-});  
+});
+
+var example1 = new Vue({
+  el: '#count-btn-example',
+  data: {
+    counter: 0
+  }
+});
