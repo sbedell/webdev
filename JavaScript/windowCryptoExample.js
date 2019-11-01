8d48c9a3ad09b1095961d976b9016c7e3b3b0a67
Steve B <bedell.369@osu.edu>
2019-11-01 10:58:00 -0400

    Updating window.crypto example to ES6 - vars to lets

--- JavaScript/windowCryptoExample.js
+++ JavaScript/windowCryptoExample.js
@@ -1,13 +1,10 @@
-var array = new Uint32Array(10);
-var arr = new Int32Array(10);
-var uint = new Uint16Array(15);
-window.crypto.getRandomValues(uint);
+let uint16Arr = new Uint16Array(15);
+let uint32Arr = new Uint32Array(10);
+
+window.crypto.getRandomValues(uint32Arr);
 
 console.log("\nYour lucky numbers:");
-// for (var i = 0; i < array.length; i++) {
-//     console.log(array[i]);
-// }
 
-for (var x of uint) {
+for (let x of uint32Arr) {
   console.log(x);
 }
