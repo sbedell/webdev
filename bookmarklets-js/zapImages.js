580cda0179ab6c15281fa27594f484d7627548eb
Steve B <bedell.369@osu.edu>
2020-05-17 17:43:00 -0400

    Moving around code on zapImages.js

--- bookmarklets-js/zapImages.js
+++ bookmarklets-js/zapImages.js
@@ -1,4 +1,20 @@
-// v1, works well, replaces images with their alt text:
+/**
+ * Bookmarklet to remove images from a web page and replace them with their alt-text
+ * Old version was not written by me. New version is my edits to the old version.
+ */
+
+(function() {
+  document.querySelectorAll('img').forEach(image => {
+    let altText = document.createTextNode(image.alt);
+    image.parentNode.replaceChild(altText, image);
+  });
+})();
+
+// One liner from above, this works:
+// (function(){document.querySelectorAll('img').forEach(image => {let altText=document.createTextNode(image.alt);image.parentNode.replaceChild(altText,image);});})();
+
+
+// Old / original version, works well, replaces images with their alt text:
 // javascript:(function(){function%20toArray%20(c){var%20a,%20k;a=new%20Array;for%20(k=0;%20k%20<%20c.length;%20++k)a[k]=c[k];return%20a;}var%20images,%20img,%20altText;images=toArray(document.images);for%20(var%20i=0;%20i%20<%20images.length;%20++i){img=images[i];altText=document.createTextNode(img.alt);img.parentNode.replaceChild(altText,%20img)}})();
 
 // Full function v1 pretty printed:
@@ -17,14 +33,3 @@
     img.parentNode.replaceChild(altText, img)
   }
 })();
-
-// Full function v2, this def works:
-(function() {
-  document.querySelectorAll('img').forEach(image => {
-    let altText = document.createTextNode(image.alt);
-    image.parentNode.replaceChild(altText, image);
-  });
-})();
-
-// So this one liner should work:
-// (function(){document.querySelectorAll('img').forEach(image => {let altText=document.createTextNode(image.alt);image.parentNode.replaceChild(altText,image);});})();
