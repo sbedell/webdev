0bab36054baba26d38850648c9d917a1bcd5b616
Steve B <bedell.369@osu.edu>
2020-05-18 13:35:55 -0400

    Removing the void call on iframeKiller.js and making it an iife instead

--- bookmarklets-js/iframeKiller.js
+++ bookmarklets-js/iframeKiller.js
@@ -6,13 +6,13 @@
 // javascript:void(function(){setInterval(function(){document.querySelectorAll('iframe').forEach(function(element){console.log('Iframe Killa - Removing Element:', element);element.parentNode.removeChild(element);})},100)}());
 
 // My edit: ( Changed element.parentNode.removeChild(element) to element.remove() and removed the setInterval )
-// javascript:void(function(){document.querySelectorAll('iframe').forEach(function(element){console.log('Iframe Killa - Removing Element:', element);element.remove()})}());
+// javascript:(function(){document.querySelectorAll('iframe').forEach(element => {console.log('Iframe Killer - Removing Element:', element);element.remove()})})();
 
 // Readable function (why even set an interval? for recurring iframes? how common is that?)
 function iframeKiller() {
   //setInterval(function() {
   document.querySelectorAll('iframe').forEach(element => {
-    console.log('Iframe Killa - Removing Element:', element);
+    console.log('Iframe Killer - Removing Element:', element);
     element.remove();
   });
   // }, 100);
