42a0f126dba09759e95ccc39b0bb171415a251d8
Steve B <bedell.369@osu.edu>
2020-06-14 13:11:20 -0400

    Updating 2 bookmarklets to open in new tabs instead of the current tab

--- bookmarklets-js/isThisSiteDown.js
+++ bookmarklets-js/isThisSiteDown.js
@@ -1,1 +1,5 @@
+// Original version, opens in same window
 javascript:document.location="https://downforeveryoneorjustme.com/"+document.location.href.split("/")[2];
+
+// Version 2, opens in a new window:
+javascript:(function(){window.open("https://downforeveryoneorjustme.com/"+document.location.href.split("/")[2]);})();
--- bookmarklets-js/tinyUrl.js
+++ bookmarklets-js/tinyUrl.js
@@ -1,1 +1,5 @@
+// Original version, this just opens it in the same tab and navigates you away from the page you're on. Not always ideal.
 javascript:document.location="https://tinyurl.com/create.php?url="+document.location.href;
+
+// Open it in a new window entirely, need to wrap in an IEFE:
+javascript:(function(){window.open("https://tinyurl.com/create.php?url="+document.location.href);})();
