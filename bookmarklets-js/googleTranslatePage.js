b215abfddc06cf00e8962a0e7995bdf32f7e9a68
Steve B <bedell.369@osu.edu>
2020-06-19 14:53:22 -0400

    Updating bookmarklets js

--- bookmarklets-js/googleTranslatePage.js
+++ bookmarklets-js/googleTranslatePage.js
@@ -1,1 +1,8 @@
+// Original, opens in same tab
 javascript:document.location='https://translate.google.com/translate?u='+encodeURIComponent(document.location);
+
+// v2, opens in new window:
+javascript:(function(){window.open('https://translate.google.com/translate?u='+encodeURIComponent(document.location));})();
+
+// OR
+(function(){window.open('https://translate.google.com/translate?u='+document.location.href);})();
