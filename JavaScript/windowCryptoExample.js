6cb3691daca9cc10fdaf442a5fcbc1989ad9f395
Steve B <bedell.369@osu.edu>
2021-06-04 12:38:36 -0400

    Adding more examples to windowCryptoExample.js

--- JavaScript/windowCryptoExample.js
+++ JavaScript/windowCryptoExample.js
@@ -1,10 +1,38 @@
+/**
+ * https://developer.mozilla.org/en-US/docs/Web/API/Web_Crypto_API
+ * https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/generateKey
+ * https://developer.mozilla.org/en-US/docs/Web/API/Crypto/getRandomValues
+ */
+
 let uint16Arr = new Uint16Array(15);
 let uint32Arr = new Uint32Array(10);
 
-window.crypto.getRandomValues(uint32Arr);
+getCryptoRandomValues(uint32Arr);
 
-console.log("\nYour lucky numbers:");
+function getCryptoRandomValues(typedArray) {
+  if (!window.isSecureContext) { console.error("Not secure context!"); }
 
-for (let x of uint32Arr) {
-  console.log(x);
+  if (window.crypto) {
+    // window.crypto.getRandomValues modifies the array itself, returns nothing.
+    window.crypto.getRandomValues(typedArray);
+    console.log(typedArray);
+  } else {
+    console.error("window.crypto not supported");
+  }
+}
+
+function generateRSAKey() {
+  window.crypto.subtle.generateKey(
+    {
+      name: "RSA-OAEP",
+      modulusLength: 4096,
+      publicExponent: new Uint8Array([1, 0, 1]),
+      hash: "SHA-256"
+    },
+    true,                   // boolean whether or not to make the keys extractable / exportable
+    ["encrypt", "decrypt"]   // key usages
+  ).then(keyPair => {
+    console.log("keyPair.publicKey: ", keyPair.publicKey);
+    console.log("keyPair.privateKey: ", keyPair.privateKey);
+  });
 }
