abe294f02dabab932208e26f998855412f61d276
sbedell <wookiesb90@gmail.com>
2025-12-22 16:51:35 -0500

    Updating bookmarksChecker.js - Attempt at producing JSON output, decent so far but not quite where I want it

--- node.js/bookmarksChecker.js
+++ node.js/bookmarksChecker.js
@@ -11,32 +11,59 @@ const https = require("https");
 // --------------------- Functions --------------
 
 function checkBookmarkForIssues(bookmark, folderName) {
-  if (bookmark.uri && bookmark.uri.match(/fbclid|gclid|utm_/)) {
-    console.log(`Query Param found: ${bookmark.uri},${bookmark.title},${folderName}`);
-  } else if (bookmark.uri && bookmark.uri.match(/^http:\/\//)) {
-    console.log(`HTTP found: ${bookmark.uri},${bookmark.title},${folderName}`);
-  } else if (bookmark.uri && bookmark.uri.match(/^https:\/\//)) {
-    checkBookmarkHttpsRequest(bookmark.uri, bookmark.title, folderName);
-  } else {} // idk? skip?
-}
-
-function checkBookmarkHttpsRequest(httpsURL, urlTitle="", folderName="") {
-  const options = {
-    timeout: 2000 // This is in milliseconds. 2 second timeout. 
+  let bookmarkWithInfo = {
+    url: bookmark.uri,
+    title: bookmark.title,
+    folder: folderName
   };
 
-  const statusCodesToCheck = [400, 401, 404]
+  if (bookmark.uri && bookmark.uri.match(/fbclid|gclid|utm_/)) {
+    // console.log(`${bookmark.uri},${bookmark.title},${folderName},Query Param Found`);
+    bookmarkWithInfo.queryParam = true;
+  } else if (bookmark.uri && bookmark.uri.match(/^http:\/\//)) {
+    console.log(`${bookmark.uri},${bookmark.title},${folderName}, HTTP URL Found`);
+    bookmarkWithInfo.insecureHttp = true;
+  } else if (bookmark.uri && bookmark.uri.match(/^javascript:/)) {
+    // Return here, don't need to do an HTTP request for bookmarklets
+    // skip?
+    // return bookmarkWithInfo;
+  } else if (bookmark.uri && bookmark.uri.match(/^https:\/\//)) {
+    // bookmarkWithInfo.errorInfo =
+    checkBookmarkHttpsRequest(bookmark, folderName);
+  }
 
-  https.get(httpsURL, options, response => {
+  return bookmarkWithInfo;
+}
+
+function checkBookmarkHttpsRequest(bookmark, folderName="") {
+  const options = {
+    timeout: 2000 // This is in milliseconds. 2 second timeout.
+  };
+
+  // Adding 403 but might want to remove it? idk yet.
+  const statusCodesToCheck = [400, 401, 403, 404];
+
+  https.get(bookmark.uri, options, response => {
     if (statusCodesToCheck.includes(response.statusCode)) {
-      console.log(`${httpsURL.substring(0, 100)},${urlTitle.replace(/,/g, " ").substring(0, 80)},${folderName},${response.statusCode}`);
+      console.log(`${bookmark.uri.substring(0, 100)},${bookmark.title.replace(/,/g, " ").substring(0, 80)},${folderName},${response.statusCode}`);
+      // return response.statusCode;
     }
+
+    // The whole response has been received. Process the result.
+    // response.on('end', () => {
+    //   if (statusCodesToCheck.includes(response.statusCode)) {
+    //   }
+    // });
   }).on('error', e => {
-    console.log(`Error checking ${httpsURL.substring(0, 100)},${urlTitle.replace(/,/g, " ").substring(0, 80)},${folderName},${e}`);
+    console.log(`${bookmark.uri.substring(0, 100)},${bookmark.title.replace(/,/g, " ").substring(0, 80)},${folderName},[!]Error: ${e}`);
   });
 }
 
-// -------------------------"Main Program:"---------------------------------
+// function consolePrint(bookmark, folderName="", errors="") {
+//   console.log(`${bookmark.uri.substring(0, 100)},${bookmark.title.replace(/,/g, " ").substring(0, 80)},${folderName},${errors}`);
+// }
+
+// ------------------------ "Main Program:" --------------------------------
 
 if (process.argv[2]) {
   fs.readFile(process.argv[2], (err, data) => {
@@ -47,23 +74,32 @@ if (process.argv[2]) {
       let bookmarksFolders = bookmarksBackup.children[0].children;
 
       // Printing in CSV file output, > pipe it to a csv file in the command line
-      console.log("URL,Title,Folder,HTTP-Status-Code");
+      console.log("URL,Title,Folder,Error(s)");
 
       for (let bookmarksFolder of bookmarksFolders) {
         // Here it is...the actual bookmarks, except when there are subfolders.
         let bookmarks = bookmarksFolder.children;
+
         if (bookmarks) {
           for (let bookmark of bookmarks) {
             // Check for Subfolders:
             if (bookmark.children) {
               for (let childBookmark of bookmark.children) {
-              //bookmark.children.forEach(childBookmark => {
-                // console.log("Child bookmark: ", childBookmark.title);
+                // Extra loop or recursion for more subfolders??? Maybe.
+
+                // Skip empty URLs
+                if (!childBookmark.uri) {
+                  continue;
+                }
+
                 checkBookmarkForIssues(childBookmark, bookmark.title);
-              //});
               }
             } else {
               // console.log("Bookmark: ", bookmark.title);
+              if (!bookmark.uri) {
+                continue;
+              }
+
               checkBookmarkForIssues(bookmark, bookmarksFolder.title);
             }
           }
